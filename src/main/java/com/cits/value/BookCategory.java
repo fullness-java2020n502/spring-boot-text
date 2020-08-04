216dc11759bddd4c64938c84d0e3d28425897f77
高橋良輔 <r-takahashi@image-pit.com>
2020-08-04 16:48:48 +0900

    Mybatisのテーブル結合によるcollectionタグとconstructタグの利用サンプル（Lombokの@RquiredArgConstructor利用）

--- src/main/java/com/cits/value/BookCategory.java
+++ src/main/java/com/cits/value/BookCategory.java
@@ -5,20 +5,17 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 /**
  * 書籍カテゴリのエンティティ
  * @author imagepit
  */
 @Data
-//@AllArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor // <-必須の引数を決められるコンストラクタ
 public class BookCategory implements Serializable{
-	private Integer id;
-	private String name;
+	private final Integer id; // <- 必須の引数
+	private final String name; // <- 必須の引数
 	private List<Book> books; // 「書籍カテゴリ」と「書籍」は「1 対 多」の関係したフィールド
-	// コンストラクタ
-	public BookCategory(Integer id, String name) {
-		this.id = id;
-		this.name = name;
-	}
 }
