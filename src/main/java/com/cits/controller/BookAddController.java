44fed6e28e1772e903a4432150e2a88985f0fea1
高橋良輔 <r-takahashi@image-pit.com>
2020-08-06 10:44:32 +0900

    SpringMVC バリデーションを追加

--- src/main/java/com/cits/controller/BookAddController.java
+++ src/main/java/com/cits/controller/BookAddController.java
@@ -1,7 +1,12 @@
 package com.cits.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +26,7 @@ import com.cits.form.BookForm;
 @RequestMapping("book-add")
 @SessionAttributes("bookAddForm") // <- bookAddFormという名前のmodel属性名がセッションスコープになる
 public class BookAddController {
+
 	// modelオブジェクトの初期化
 	@ModelAttribute("bookAddForm")
 	public BookForm initBook() {
@@ -30,16 +36,35 @@ public class BookAddController {
 	public String input() {
 		return "book-add/input";
 	}
+	// HttpSessionはDIで呼び出せます
+	@Autowired HttpSession session;
+
+
 	@PostMapping("confirm")
-	public String confirm(@ModelAttribute("bookAddForm") BookForm bookAddForm) {
+	public String confirm(
+		// ↓ 2. バリデートする対象に@Validatedを付与
+		@Validated @ModelAttribute("bookAddForm") BookForm bookAddForm,
+		// ↓ 3. バリデート結果を格納するオブジェクトを引数に用意
+		BindingResult result
+	) {
+		// 4. バリデート結果の評価
+		if(result.hasErrors()) return "book-add/input";
+		// セッションに格納
+		// session.setAttribute("sessForm", bookAddForm);
 		return "book-add/confirm";
 	}
+
+
 	@PostMapping("execute")
 	public String Execute(
 			@ModelAttribute("bookAddForm") BookForm bookForm,
 			RedirectAttributes attributes
 		) {
 		System.out.println("bookForm:" + bookForm);
+		// セッションのデータを取得
+		// BookForm bookForm2 =  (BookForm)session.getAttribute("sessForm");
+		// System.out.println("bookForm2"+bookForm2);
+
 		attributes.addFlashAttribute("bookForm",bookForm);
 		// TODO DB登録処理
 		return "redirect:complete"; // <- リダイレクト（PRG）
@@ -47,6 +72,8 @@ public class BookAddController {
 	@GetMapping("complete")
 	public String complete(SessionStatus status, BookForm bookForm, Model model) {
 		status.setComplete(); // <- セッションを破棄
+		// session.removeAttribute("sessForm");
+		// session.invalidate();
 		model.addAttribute("bookForm",bookForm);
 		return "book-add/complete";
 	}
--- src/main/java/com/cits/form/BookForm.java
+++ src/main/java/com/cits/form/BookForm.java
@@ -2,10 +2,18 @@ package com.cits.form;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class BookForm implements Serializable{
+	// 1. バリデーション（単項目）をするためにはフィールドの上にバリデーションのアノテーションを付与
+	@Size(max = 10)// 文字数を10桁
+	@NotBlank // nullや空白は許可しない
 	private String title;
+	@Size(max = 8)
+	@NotBlank
 	private String author;
 }
